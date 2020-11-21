19c948fcb33675750d51432efba776ba722ef2d6
Nastik1811 <lugovskaya.1999@mail.ru>
2020-11-21 12:54:27 +0300

    Update navbar

--- src/components/UserMenu/index.tsx
+++ src/components/UserMenu/index.tsx
@@ -1,1 +1,40 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
+
+const UserMenu = ({className}: any) => {
+    return(
+    <nav>
+        <ul className={className}>
+            <li>
+                <NavLink 
+                    className="menu-link" 
+                    activeClassName="menu-link active" 
+                    to="/home/in_progress">
+                        My courses
+                </NavLink>
+            </li>
+            <li>
+                <NavLink 
+                    className="menu-link" 
+                    activeClassName="menu-link active" 
+                    to="/home/own_courses">
+                        Created courses
+                </NavLink>
+            </li>
+            <li>
+                <NavLink 
+                    className="menu-link" 
+                    activeClassName="menu-link active" 
+                    to="/home/achivements">
+                        My Achievements
+                </NavLink>
+            </li>
+            <li>
+                <button className="menu-link logout-btn">Log out</button>
+            </li>
+        </ul>
+    </nav>
+    )
+}
+
+export default UserMenu
